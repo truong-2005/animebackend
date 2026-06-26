b9823af348128489ceb6a8ba7bb9aeafcddf2bb4
truong-2005 <163482327+truong-2005@users.noreply.github.com>
2026-06-26 07:52:48 +0700

    Initial commit

--- src/main/java/com/qtanime/animebackend/controller/NotificationController.java
+++ src/main/java/com/qtanime/animebackend/controller/NotificationController.java
@@ -1,25 +1,36 @@
 package com.qtanime.animebackend.controller;
 
-import com.qtanime.animebackend.dto.common.MessageResponse;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qtanime.animebackend.entity.Notification;
+import com.qtanime.animebackend.service.NotificationService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/notifications")
+@RequiredArgsConstructor
 public class NotificationController {
 
-    @GetMapping
-    public MessageResponse getNotifications() {
+    private final NotificationService notificationService;
 
-        return MessageResponse.builder()
-                .message("Danh sách thông báo")
-                .build();
+    @GetMapping
+    public List<Notification> getNotifications() {
+
+        return notificationService.getAll();
     }
 
     @PostMapping("/send")
-    public MessageResponse sendNotification() {
+    public Notification sendNotification(
+            @RequestBody Notification notification
+    ) {
 
-        return MessageResponse.builder()
-                .message("Gửi thông báo thành công")
-                .build();
+        return notificationService.create(notification);
     }
 }
